b90e5ba9169538b155a263c0db509567284c4d1b
LetsGoPewPew <stian.dahlstrom@hotmail.com>
2018-10-19 19:38:45 +0200

    added files

---
+++ app/src/main/java/no/hiof/stianad/cachemeifyoucan/Achievement.java
@@ -0,0 +1,12 @@
+package no.hiof.stianad.cachemeifyoucan;
+
+public class Achievement {
+    public String name;
+    public String description;
+
+    public Achievement(String name, String description)
+    {
+        this.description = description;
+        this.name = name;
+    }
+}
