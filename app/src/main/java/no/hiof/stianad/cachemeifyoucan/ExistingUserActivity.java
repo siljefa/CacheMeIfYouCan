d1781ba36079c7868e52800e2ac8d8c16e36b378
LiseHiof <lise.hauslo@gmail.com>
2018-11-09 12:38:44 +0100

    Restructured loginActivity

--- app/src/main/java/no/hiof/stianad/cachemeifyoucan/ExistingUserActivity.java
+++ app/src/main/java/no/hiof/stianad/cachemeifyoucan/ExistingUserActivity.java
@@ -2,6 +2,7 @@ package no.hiof.stianad.cachemeifyoucan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ExistingUserActivity extends AppCompatActivity {
 
--- app/src/main/java/no/hiof/stianad/cachemeifyoucan/LoginActivity.java
+++ app/src/main/java/no/hiof/stianad/cachemeifyoucan/LoginActivity.java
@@ -12,10 +12,38 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnSignup;
+    private Button btnExistingUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignup = (Button) findViewById(R.id.activity_login_btn_signUp);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+
+        btnExistingUser = (Button) findViewById(R.id.activity_existingUser_btn_login);
+        btnExistingUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openExiStingUserActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity(){
+        Intent intent1 = new Intent(this, SignUpActivity.class);
+        startActivity(intent1);
+    }
+
+    public void openExiStingUserActivity(){
+        Intent intent2 = new Intent(this, ExistingUserActivity.class);
+        startActivity(intent2);
     }
 }
