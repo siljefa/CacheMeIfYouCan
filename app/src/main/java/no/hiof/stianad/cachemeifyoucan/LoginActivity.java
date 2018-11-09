ecc10c855fd45b235289df267d469a4dece05297
LiseHiof <lise.hauslo@gmail.com>
2018-11-09 12:44:40 +0100

    Restructured loginActivity

--- app/src/main/java/no/hiof/stianad/cachemeifyoucan/LoginActivity.java
+++ app/src/main/java/no/hiof/stianad/cachemeifyoucan/LoginActivity.java
@@ -12,22 +12,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnSignup;
-    private Button btnExistingUser;
+    private Button btnSignup;//For user signup
+    private Button btnExistingUser;//For existing user accounts
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //When button is clicked it takes the user to the signup activity
         btnSignup = (Button) findViewById(R.id.activity_login_btn_signUp);
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openLoginActivity();
+                openSignUpActivity();
             }
         });
 
+        //When button is clicked it takes user to existing user activity
         btnExistingUser = (Button) findViewById(R.id.activity_existingUser_btn_login);
         btnExistingUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,12 +38,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
-    public void openLoginActivity(){
+    
+    //Method that start loginActivity
+    public void openSignUpActivity(){
         Intent intent1 = new Intent(this, SignUpActivity.class);
         startActivity(intent1);
     }
 
+    //Method that starts ExistingUseractivity
     public void openExiStingUserActivity(){
         Intent intent2 = new Intent(this, ExistingUserActivity.class);
         startActivity(intent2);
