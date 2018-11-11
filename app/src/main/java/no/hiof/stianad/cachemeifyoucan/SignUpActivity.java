cb834c358ee3f827a03bda8d75787ad78d9c063e
LiseHiof <lise.hauslo@gmail.com>
2018-11-11 16:17:34 +0100

    Logo added to loginActivity

--- app/src/main/java/no/hiof/stianad/cachemeifyoucan/SignUpActivity.java
+++ app/src/main/java/no/hiof/stianad/cachemeifyoucan/SignUpActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
-    private Button btnSignUp;//Sign up button
+    private Button btnSignUp; //Sign up button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
