3509d81efe38c298348efe1a6ddd8bd89dc01e6a
Knut Egge <knut97@hotmail.com>
2018-11-13 17:13:15 +0100

    Moved code out of map fragment

---
+++ app/src/main/java/no/hiof/stianad/cachemeifyoucan/Achievements/Achievement.java
@@ -0,0 +1,12 @@
+package no.hiof.stianad.cachemeifyoucan.Achievements;
+
+public class Achievement {
+    public String name;
+    public String description;
+
+    public Achievement(String name, String description)
+    {
+        this.description = description;
+        this.name = name;
+    }
+}
