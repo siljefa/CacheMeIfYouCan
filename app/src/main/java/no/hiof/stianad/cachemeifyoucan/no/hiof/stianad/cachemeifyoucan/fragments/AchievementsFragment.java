e3a06da864288e9b76378a4dcf83c5df9bae7c33
LetsGoPewPew <stian.dahlstrom@hotmail.com>
2018-11-16 00:04:30 +0100

    Importing achievements from database - NOT WORKING -

--- app/src/main/java/no/hiof/stianad/cachemeifyoucan/no/hiof/stianad/cachemeifyoucan/fragments/AchievementsFragment.java
+++ app/src/main/java/no/hiof/stianad/cachemeifyoucan/no/hiof/stianad/cachemeifyoucan/fragments/AchievementsFragment.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 import no.hiof.stianad.cachemeifyoucan.R;
@@ -41,16 +47,37 @@ public class AchievementsFragment extends Fragment{
         listView.setAdapter(adapter);
 
         ArrayList<Achievement> newAchievements = new ArrayList<>();
-        Achievement ach1 = new Achievement("10k", "walk 10000 metres");
-        Achievement ach2 = new Achievement("20k", "walk 20000 metres");
-        Achievement ach3 = new Achievement("30k", "walk 30000 metres");
-        Achievement ach4 = new Achievement("40k", "walk 40000 metres");
+        /*newAchievements.add(new Achievement("10k", "walk 10000 metres"));
+        newAchievements.add(new Achievement("20k", "walk 20000 metres"));
+        newAchievements.add(new Achievement("30k", "walk 30000 metres"));
+        */
 
-        newAchievements.add(ach1);
-        newAchievements.add(ach2);
-        newAchievements.add(ach3);
-        newAchievements.add(ach4);
+        newAchievements = FillArrayListWithAchievements();
 
         adapter.addAll(newAchievements);
+    }
+
+    private ArrayList<Achievement> FillArrayListWithAchievements(){
+        ArrayList<Achievement> list = new ArrayList<>();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
+        ref.child("achievement").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                for (DataSnapshot child: children) {
+                    Achievement achievement = child.getValue(Achievement.class);
+                    //adding achievements to list
+                    list.add(achievement);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return list;
     }
 }
